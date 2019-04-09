7dae2f5131c889953b98da9f6de725f8dafdebf1
kishankt <31003331+kishankt@users.noreply.github.com>
2019-04-10 02:05:56 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,43 +1,20 @@
 #include<stdio.h>
-
 #include<stdlib.h>
-
-
 int main ()
-
 {
-
 int pReq[15],i,j,cylinder,curPossition,prevPossition,qSize,qInput,qList[15],sortQueList[15],t,calc,sum=0,rev,result;
-
-
 printf("Please enter the max size of cylinder disk drive : ");
-
 scanf("\n%d",&cylinder);
-
 printf("Please enter the current position of Serving : ");
-
 scanf("\n%d",&curPossition);
-
 printf("Please enter the pending queue list size : ");
-
 scanf("\n%d",&qSize);
-
-
 printf("Please enter the pending %d queue Number : ",qSize);
-
 for (i = 0; i<=qSize-1; i++)
-
 {
-
 scanf("%d",&qInput); //taking pending queue list input from user.
-
 if (qInput>0)
-
 {
-
 qList[i]=qInput;
-
 }
-
-
 }
